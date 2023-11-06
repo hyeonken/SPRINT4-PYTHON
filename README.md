0b9e492f0194bf6d3705fb3a343ad536adb0cb74
Rony Ken <126473293+hyeonken@users.noreply.github.com>
2023-11-06 14:41:39 -0300

    Sprint3

---
+++ README.md
@@ -0,0 +1,33 @@
+# Sprint 3 - Python:
+Repositórios referente a terceira sprint da challenge proposto pela FIAP em 2023.
+
+--
+
+# Conheça a iniciativa Aquatank!
+O grupo SoftForge está desenvolvendo um projeto que vai mudar a forma que respiramos nas grandes cidades, com previsão de lançamento para o segundo semestre de 2023!
+
+Inscreva-se em nosso formulário para conhecer a iniciativa antes de todos 🚀
+
+--
+
+# Features Implementadas:
+Melhora na área de cadastro (mais funcionalidades);
+Adicinado um link de Id para iniciar o sitema Aquatank;
+Mostrar a última atualização do arduino;
+Inclusão de um suporte especializado;
+Opção de ver todas as ações do usuário percorrendo o menu e dando a ele a escolha de finalizar o programa ou continuar acessando o menu;
+Foi inserido dentro do menu uma opção de encerrar o programa.
+
+--
+
+# Pessoas desenvolvedoras:
+
+⚠ **Atenção! Esse grupo possui integrantes em salas diversas** ⚠
+
+| Aluno  | Rm |  Turma |
+| ----------------- | ------------- |---------|
+| Gustavo Cristiano | 551924  |1ESPF|
+| Luna Gouvea  | 550836  |1ESPV|
+| Rony Ken Nagai  | 551549  |1ESPF|
+| Pedro P. Santos| 552047  |1ESPF|
+| Tomáz V Carballo | 551417  |1ESPF|
---
+++ sprint3-python.py
@@ -0,0 +1,196 @@
+lista = []
+cadastro_feito = 0
+login_feito = 0
+dicionario = {}
+import datetime
+import time
+
+def menu_aquatank1():
+    if cadastro_feito == 0 or login_feito == 0:
+        print('----------------------------------------------')
+        print('                  \033[34mAquatank\033[m                ')
+        print('----------------------------------------------\n')
+        print('1 - Cadastro no site da Aquatank')
+        print('2 - Login no site da Aquatank')
+        print('3 - Encerrar o programa\n')
+        print('----------------------------------------------\n')
+        try:
+            escolha_menu1 = int(input("Escolha uma dessas duas opções: "))
+            print()
+        except ValueError:
+            print("\033[31mDigite um número inteiro!\033[m\n")   
+        return escolha_menu1
+ 
+def menu_aquatank2():
+    while True:
+        try:
+            print('----------------------------------------------')
+            print('                  \033[34mAquatank\033[m                ')
+            print('----------------------------------------------\n')
+            print('1 - Ver a última atualização do Arduino')
+            print('2 - Suporte especializado')
+            print('3 - Mostrar todas as operações realizadas')
+            print('4 - Encerrar o programa\n')
+            print('----------------------------------------------\n')
+
+            escolha_menu2 = int(input("Escolha uma dessas duas opções: "))
+            print()
+        
+            match escolha_menu2:
+                case 1: 
+                    print("última atualização dos componentes e do arduino da Aquatank:") 
+                    print("                    --                      ")
+                    print("BlackBoard UNO R3 - ROBODORE")  
+                    print("Placa DOIT ESP32 (Bluetooth e Wifi)")
+                    print("Sensor Nivel Lateral Água Arduino - Tipo Boia")
+                    print("Displey Lcd 1602 16x2")
+                    print("Módulo Sensor de Luz (ldr)")
+                    print("Led RGB")
+                    print("Buzzer Arduíno")
+                    print("Sensor de Qualidade do Ar - C02")
+                    print("                    --                      \n")
+                    lista.append('Ver a última atualização do Arduino')
+                case 2:
+                    alguma_duvida = input("Você tem alguma dúvida sobre o Aquatank? (sim/não): ").lower()
+                    print()
+                    if   alguma_duvida == "sim" or alguma_duvida == "s":
+                        for i in range(5):
+                            i = "."
+                            print(i)
+
+                        num = int(input("Digite a quantidade de dúvidas: "))
+                        print()
+                        contador = 0
+                        while contador < num:
+                            duvida = input(f"Escreva sua {contador + 1}ª dúvida ao lado: ")
+                            print(f"{duvida}\n")
+                            contador += 1
+                            dicionario[contador] = duvida 
+                            print(dicionario)
+                            
+                        print("\nAgradecemos pelas dúvidas. Ela será analisada e retornada em breve no seu e-mail...\n")
+                        lista.append("Suporte especializado - Teve dúvida")
+                     
+                    else:  
+                        lista.append("Suporte especializado - Não teve nenhuma dúvida")  
+                case 3: 
+                    print()
+                    resposta = input("Deseja ver o resumo de operações realizadas do menu? (sim/não): ").lower()
+                    print()
+                    if resposta == "não" or resposta == "n" or resposta == "nao":
+                        print("Obrigado por utilizar o programa!")   
+                    elif resposta == "sim" or resposta == "s":
+                        print("Resumo das operações realizadas:")
+                        print()
+                        for n in lista:
+                            print(f"\033[34m{n}\033[m")
+                    print()
+                    resposta2 = input("Deseja continuar? (sim/não): ").lower()
+                    print() 
+                    if resposta2 == "não" or resposta2 == "n" or resposta2 == "nao":
+                        print("Obrigado pela compreensão! Te esperamos em breve novamente!\n") 
+                        return True
+                    else:
+                        lista.append("Mostrar todas as operações realizadas")        
+                case 4:
+                    return True
+                case _:
+                    print("\033[31mError!! Número inválido!\033[m \n")
+        except ValueError:
+            print("\033[31mDigite um número inteiro!\033[m\n")        
+
+def validar_email(email):
+    # Aqui você pode adicionar sua lógica de validação de email
+    # Por exemplo, verificar se o email possui um "@" e um "."
+    return "@" in email 
+
+# Função para verificar a validade da senha
+def validar_senha(senha):
+    # Aqui você pode adicionar sua lógica de validação de senha
+    # Por exemplo, verificar se a senha tem pelo menos 8 caracteres
+    return len(senha) >= 8
+
+while True:
+    escolha_menu1 = menu_aquatank1() 
+
+    if escolha_menu1 == 1:
+        current_time = datetime.datetime.now()
+        print("Hora atual:", current_time.strftime("%H:%M:%S"))
+        time.sleep(1)
+
+        email_valido = False
+        senha_valida = False
+
+        while not (email_valido and senha_valida):
+            nome = input('Digite o seu nome: ')
+            email = input('Informe seu e-mail: ')
+            senha = input('Digite sua senha(8 caracteres): ')
+
+            email_valido = validar_email(email)
+            senha_valida = validar_senha(senha)
+
+            if not email_valido and not senha_valida:
+                print("\n\033[31mEmail e senha inválidos! Tente novamente!\033[m\n")
+            elif not email_valido:
+                print("\n\033[31mEmail inválido! Tente novamente!\033[m\n")
+            elif not senha_valida:    
+                print("\n\033[31mSenha inválida! Tente novamente!\033[m\n")
+     
+        print(f'\nOlá! Poderia confirmar se está certo seu e-mail: {email}\n')
+                
+        confirmacao = input('Digite (sim/não): ')
+        if confirmacao == "s" or confirmacao == "sim" or confirmacao == "Sim" or confirmacao == "S":
+
+            if '@' in email and len(senha)>=8:
+                print("\n\033[32mCadastro realizado com sucesso!\033[m\n")
+                print(f"Seja bem vindo {nome}!\n")
+                cadastro_feito = 1
+                lista.append('Cadastro no site da Aquatank')
+                retorno = menu_aquatank2()
+                if retorno == True:
+                    break     
+        else:
+            print()
+            print("\033[31mTente outra vez mais tarde!\033[m\n")  
+
+    elif escolha_menu1 == 2:
+        current_time = datetime.datetime.now()
+        print("Hora atual:", current_time.strftime("%H:%M:%S"))
+        time.sleep(1)
+        email_valido = False
+        senha_valida = False
+        while not (email_valido and senha_valida):
+            email = input('Informe seu e-mail: ')
+            senha = input('Digite sua senha(8 caracteres): ')
+
+            email_valido = validar_email(email)
+            senha_valida = validar_senha(senha)
+
+            if not email_valido and not senha_valida:
+                print("\n\033[31mEmail e senha inválidos! Tente novamente!\033[m\n")
+            elif not email_valido:
+                print("\n\033[31mEmail inválido! Tente novamente!\033[m\n")
+            elif not senha_valida:    
+                print("\n\033[31mSenha inválida! Tente novamente!\033[m\n")
+
+        #Chamar para ele voltar para cá.
+        if'@' in email and len(senha)>=8:
+            print("\nVerificando sua conta...")
+            for i in range(5):
+                i = "."
+                print(i)
+            print("Sincronizando com a sua conta...")
+            for i in range(5):
+                i = "."
+                print(i)
+            print("\n\033[32mSeja bem-vindo ao Aquatank!\033[m\n")
+            login_feito = 1
+            lista.append('Login no site da Aquatank.')
+            retorno = menu_aquatank2()
+            if retorno == True:
+                break
+            
+    elif escolha_menu1 == 3:
+        break
+    else:
+        print("\033[31mError!! Número inválido!\033[m \n")
